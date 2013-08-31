b6a409b50ba4e6e2451ece646f281eb040ad13ce
Stefan Muntwyler <stefan.muntwyler@gmail.com>
2013-08-31 19:52:31 +0200

    initial commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+m = Movie.create!(:title => 'Star wars', :release_date => '27-5-1977')
